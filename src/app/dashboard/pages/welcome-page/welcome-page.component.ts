3e793974017b096d1079d552002c10cb79498f3b
marckux-dev <marckux.developer@gmail.com>
2025-02-20 13:18:04 +0100

    Welcome page modified

--- src/app/dashboard/pages/welcome-page/welcome-page.component.ts
+++ src/app/dashboard/pages/welcome-page/welcome-page.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {MaterialModule} from '../../../material/material/material.module';
+import {Center} from '../../../features/centers/interfaces/center.interface';
+import {CentersService} from '../../../features/centers/services/centers.service';
+import {RouterLink} from '@angular/router';
+import {LowerCasePipe, NgForOf} from '@angular/common';
 
 @Component({
   selector: 'dashboard-welcome-page',
-  imports: [MaterialModule],
+  imports: [MaterialModule, RouterLink, NgForOf, LowerCasePipe],
   templateUrl: './welcome-page.component.html',
   standalone: true,
   styles: ``
 })
-export class WelcomePageComponent {
+export class WelcomePageComponent implements OnInit {
 
+  private centersService: CentersService = inject(CentersService);
+  centers: Center[] = [];
+
+  ngOnInit(): void {
+    this.centersService.getAll().subscribe(centers => {
+      this.centers = centers;
+    });
+  }
 }
