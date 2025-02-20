a9d9493a3f485dce63f815fa91b3d137863080f1
marckux-dev <marckux.developer@gmail.com>
2025-02-20 12:43:09 +0100

    LongPressDirective Changed

--- src/app/shared/directives/long-press.directive.ts
+++ src/app/shared/directives/long-press.directive.ts
@@ -7,20 +7,45 @@ export class LongPressDirective {
   // Delay in milliseconds (default 200ms)
   @Input() longPressDelay = 200;
   // Emit the event when the long press is triggered.
-  @Output() longPress = new EventEmitter<MouseEvent>();
+  @Output() longPress = new EventEmitter<Event>();
 
   private timer: any;
 
+  // For desktop mouse events
   @HostListener('mousedown', ['$event'])
   onMouseDown(event: MouseEvent): void {
-    this.timer = setTimeout(() => {
-      this.longPress.emit(event);
-    }, this.longPressDelay);
+    this.startTimer(event);
   }
 
   @HostListener('mouseup')
   @HostListener('mouseleave')
   cancelLongPress(): void {
-    clearTimeout(this.timer);
+    this.clearTimer();
+  }
+
+  // For mobile touch events
+  @HostListener('touchstart', ['$event'])
+  onTouchStart(event: TouchEvent): void {
+    this.startTimer(event);
+  }
+
+  @HostListener('touchend')
+  @HostListener('touchcancel')
+  onTouchEnd(): void {
+    this.clearTimer();
+  }
+
+  private startTimer(event: Event): void {
+    this.timer = setTimeout(() => {
+      this.longPress.emit(event);
+    }, this.longPressDelay);
+  }
+
+  private clearTimer(): void {
+    if (this.timer) {
+      clearTimeout(this.timer);
+      this.timer = null;
+    }
   }
 }
+
